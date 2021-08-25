e7418db9517fc92dce5ad6f7db2d1f78c95250f3
snehashish <snehashish.laskar@sahyadrischool.org>
2021-08-25 12:00:37 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# Author: Snehashish Laskar
+# Date : 25-08-2021
+# Time: 12:00
+
+# Author: Snehashish Laskar and Ishan Kashyap
+# Date : 25-08-2021
+# Time: 11:51
+
+# This is a multiplayer chess game designed to be played from the terminal
+# This uses OOP (Object Oriented Programming) to represent each piece in the game
+# There can be two connections at once playing the game the server for now runs on localhost
+
